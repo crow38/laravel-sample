3fde9f7a7ef7881969e74c87df59eefa8b92c2c0
crow38 <kd1345537@st.kobedenshi.ac.jp>
2023-09-04 13:36:18 +0900

    first commit

---
+++ resources/views/test1.blade.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>TEST</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="wrapper">
+
+        <h1>guru</h1>
+
+        </div>
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
